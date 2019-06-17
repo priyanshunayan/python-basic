c62c15403cbd472f6366729bee2e66a99d9fd7de
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-06-17 15:16:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+# character_name = "Priyanshu"
+# character_age = 35
+#
+# print("There was a man named " + character_name + ',')
+# print(character_name.upper().isupper())
+# print(character_name.replace("Priyanshu", "Appu"))
+
+# name =input("Enter your name: ")
+# age = input('Enter your age')
+# print("Hello " +name + " ! You are " + age)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+#
+# result = float(num1) + float(num2)
+#
+# print(result)
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print( plural_noun + " are blue")
+print("I love " + celebrity)
+
+
+
+
